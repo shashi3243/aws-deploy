f1bb9d2199b546e0aa719a1c3797d36394a82c39
shashikant <s.sethiya@brianinventory.com>
2024-06-09 01:09:57 +0530

    add api

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
-require("dotenv").config({path:`.env.${process.env.NODE_ENV}`})
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
-const stripe = require('stripe')(`${process.env.SECRET_KEY}`);
+const stripe = require("stripe")(`${process.env.SECRET_KEY}`);
 
-const express = require("express")
+const express = require("express");
 // require("./db")
-const {Product,User} = require("./model")
-const app = express()
-const port = process.env.PORT
+const { Product, User } = require("./model");
+const app = express();
+const port = process.env.PORT;
 
-app.get("/", (req,res)=>{
-return res.status(200).json({
-messsage:"Welcome to my apk"
-})
-})
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    messsage: "Welcome to my apk",
+  });
+});
 
-
-
-
+app.get("/data", (req, res) => {
+  return res.status(200).json({
+    messsage: "jenkins integrate successfully .",
+  });
+});
 
 // app.post('/github-webhook', (req, res) => {
 //     // console.log("")
@@ -24,38 +26,35 @@ messsage:"Welcome to my apk"
 //     res.status(200).send('Webhook received');
 // });
 
-app.post("/createProduct",async(req,res)=>{
-    const data = await stripe.products.create({
-        name: "indian product",
-      });
-      const createProductData = await Product.create({
-          productId:data?.id,
-          name:data?.name
-      })
-    return  res.json({
-        message:"ok",
-        data:createProductData
-    })
-       
-})
+app.post("/createProduct", async (req, res) => {
+  const data = await stripe.products.create({
+    name: "indian product",
+  });
+  const createProductData = await Product.create({
+    productId: data?.id,
+    name: data?.name,
+  });
+  return res.json({
+    message: "ok",
+    data: createProductData,
+  });
+});
 
-app.post("/createPrice",async(req,res)=>{
-    const price = await stripe.prices.create({
-        currency: 'inr',
-        unit_amount: 1000,
-        recurring: {
-          interval: 'year',
-        },
-        product: "prod_Q1yBWRZI9BW93I"
-      });
-      return  res.json({
-        message:"ok",
-        data:price
-    })
-})
+app.post("/createPrice", async (req, res) => {
+  const price = await stripe.prices.create({
+    currency: "inr",
+    unit_amount: 1000,
+    recurring: {
+      interval: "year",
+    },
+    product: "prod_Q1yBWRZI9BW93I",
+  });
+  return res.json({
+    message: "ok",
+    data: price,
+  });
+});
 
-
-
-app.listen(port,()=>{
-    console.log(`server is listening on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
