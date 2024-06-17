30e3ce4a71cbdcb063daf0e72728b05cff5df1a0
shashikant <s.sethiya@brianinventory.com>
2024-06-17 18:43:40 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   return res.status(200).json({
-    messsage: "I have updated jenkins for deployement.",
+    messsage: "my name  is jenkins for the updation",
   });
 });
 
